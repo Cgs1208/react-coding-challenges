fb635355e438b8964167e1fb92b9edabe8555466
Cgs1208 <charangs157@gmail.com>
2024-03-06 23:17:27 +0530

    final logic

--- grid-lights/src/App.js
+++ grid-lights/src/App.js
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import "./App.css";
 import Cell from "./components/Cell";
 
-function App() {
+export default function App() {
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+
   const config = [
     [1, 1, 1],
     [1, 0, 1],
     [1, 1, 1],
   ];
-  // 1 -> cell appears and 0 -> cell doesn't appear
 
-  const handleActivateCells = () => {};
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const timer = setInterval(() => {
+      setOrder((origOrder) => {
+        const newOrder = origOrder.slice();
+        newOrder.pop();
+
+        if (newOrder.length === 0) {
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+
+        return newOrder;
+      });
+    }, 300);
+  };
+
+  const activateCells = (index) => {
+    const newOrder = [...order, index];
+    setOrder(newOrder);
+    // deactivate
+    if (newOrder.length === config.flat(1).filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
 
   return (
     <div className="wrapper">
       <div
         className="grid"
-        style={{ gridTemplateColumns: `repeat(${config[0].length}, 1fr)` }}
+        style={{
+          gridTemplateColumns: `repeat(${config[0].length}, 1fr)`,
+        }}
       >
         {config.flat(1).map((value, index) => {
           return value ? (
             <Cell
               key={index}
-              filled={false}
-              onClick={() => handleActivateCells(index)}
+              label={`Cell ${index}`}
+              filled={order.includes(index)}
+              onClick={() => activateCells(index)}
+              isDisabled={order.includes(index) || isDeactivating}
             />
           ) : (
             <span />
@@ -32,5 +63,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
