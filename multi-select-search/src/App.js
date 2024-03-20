2876dd81b44266d80745ddd914e5740d71cf1377
Cgs1208 <charangs157@gmail.com>
2024-03-20 22:57:42 +0530

    wireframe

--- multi-select-search/src/App.js
+++ multi-select-search/src/App.js
@@ -2,7 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="user-search-input">
+      {/* pills */}
+      {/* input field with suggestions */}
+      <div>
+        <input type="text" />
+        {/* search suggestions */}
+      </div>
       <h1>hey</h1>
     </div>
   );
