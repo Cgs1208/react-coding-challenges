23abe862743c1aeda610dd5baf9ef966769728b0
Cgs1208 <charangs157@gmail.com>
2024-03-21 23:15:41 +0530

    basic styling on search input

--- multi-select-search/src/App.js
+++ multi-select-search/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <div className="user-search-input">
-      {/* pills */}
-      {/* input field with suggestions */}
-      <div>
-        <input type="text" />
-        {/* search suggestions */}
+    <div className="user-search-container">
+      <div className="user-search-input">
+        {/* pills */}
+        {/* input field with suggestions */}
+        <div>
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Search an user"
+          />
+          {/* search suggestions */}
+        </div>
       </div>
     </div>
   );
