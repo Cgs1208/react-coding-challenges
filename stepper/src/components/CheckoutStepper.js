a3c9c47a3172379d7388a1e23ecd6192e6f7c712
Cgs1208 <charangs157@gmail.com>
2024-03-24 23:08:41 +0530

    align progress bar using useRef

--- stepper/src/components/CheckoutStepper.js
+++ stepper/src/components/CheckoutStepper.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const CheckoutStepper = ({ stepsConfig = [] }) => {
   const [currentStep, setCurrentStep] = useState(1);
   const [isComplete, setIsComplete] = useState(false);
+  const [margins, setMargins] = useState({
+    marginLeft: 0,
+    marginRight: 0,
+  });
+  const stepRef = useRef([]);
+
+  useEffect(() => {
+    setMargins({
+      marginLeft: stepRef.current[0].offsetWidth / 2,
+      marginRight: stepRef.current[stepsConfig.length - 1].offsetWidth / 2,
+    });
+  }, [stepRef, stepsConfig.length]);
 
   const handleNext = () => {
     setCurrentStep((prevStep) => {
@@ -31,6 +43,7 @@ const CheckoutStepper = ({ stepsConfig = [] }) => {
           return (
             <div
               key={step.name}
+              ref={(el) => (stepRef.current[index] = el)}
               className={`step ${
                 currentStep > index + 1 || isComplete ? "complete" : ""
               }
@@ -48,7 +61,14 @@ const CheckoutStepper = ({ stepsConfig = [] }) => {
           );
         })}
 
-        <div className="progress-bar">
+        <div
+          className="progress-bar"
+          style={{
+            width: `calc(100%-${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft: margins.marginLeft,
+            marginRight: margins.marginRight,
+          }}
+        >
           <div
             className="progress"
             style={{ width: `${calculateProgressBarWidth()}%` }}
