24e98805ad33808e9769db832898f9c312793979
Cgs1208 <charangs157@gmail.com>
2024-03-24 13:24:31 +0530

    stepper component with basic styles

--- stepper/src/components/CheckoutStepper.js
+++ stepper/src/components/CheckoutStepper.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const CheckoutStepper = ({ stepsConfig = [] }) => {
+  if (!stepsConfig.length) return <></>;
   return (
     <div className="stepper">
       {stepsConfig.map((step, index) => {
         return (
-          <div key={step.name}>
-            <div>{index + 1}</div>
+          <div key={step.name} className="step">
+            <div className="step-number">{index + 1}</div>
             <div className="step-name">{step.name}</div>
           </div>
         );
