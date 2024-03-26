df158edbad3e7d7fd81eec03977009d699c3d829
Cgs1208 <charangs157@gmail.com>
2024-03-26 23:49:55 +0530

    question component

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 import questions from "./constants/constants.json";
+import Question from "./components/Question";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [usersAnswers, setUsersAnwers] = useState([]);
 
+  const handleAnswerClick = (isCorrect) => {
+    setCurrentQuestion(currentQuestion + 1);
+    setUsersAnwers([...usersAnswers, isCorrect]);
+  };
+
   return (
     <div className="App">
       <h2>Quiz</h2>
       {/* question component */}
+      {currentQuestion < questions.length && (
+        <Question
+          question={questions[currentQuestion]}
+          onAnswerClick={handleAnswerClick}
+        />
+      )}
       {/* result component  */}
     </div>
   );
--- quiz-app/src/components/Questions.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Questions = ({ question }) => {
-  return <div>Questions</div>;
-};
-
-export default Questions;
