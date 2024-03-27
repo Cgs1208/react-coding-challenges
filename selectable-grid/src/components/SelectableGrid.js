efa2fc83e51f4d78bec71104725366b179746fea
Cgs1208 <charangs157@gmail.com>
2024-03-27 23:07:05 +0530

    grid selection logic

--- selectable-grid/src/components/SelectableGrid.js
+++ selectable-grid/src/components/SelectableGrid.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SelectableGrid = ({ rows = 10, cols = 10 }) => {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [selectedBoxes, setSelectedBoxes] = useState([]);
+
+  const handleMouseDown = (boxNumber) => {
+    setIsMouseDown(true);
+    setSelectedBoxes([...selectedBoxes, boxNumber]);
+  };
+
+  const handleMouseEnter = (boxNumber) => {
+    if (isMouseDown) {
+      const startBox = selectedBoxes[0];
+      const endBox = boxNumber;
+
+      const startRow = Math.floor((startBox - 1) / cols);
+      const startCol = (startBox - 1) % cols;
+      const endRow = Math.floor((endBox - 1) / cols);
+      const endCol = (endBox - 1) % cols;
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
   return (
-    <div className="grid" style={{ "--rows": rows, "--cols": cols }}>
+    <div
+      className="grid"
+      style={{ "--rows": rows, "--cols": cols }}
+      onMouseUp={handleMouseUp}
+    >
       {[...Array(rows * cols).keys()].map((_, i) => {
         return (
-          <div key={i} className="box">
+          <div
+            key={i}
+            className="box"
+            onMouseDown={() => handleMouseDown(i + 1)}
+            onMouseEnter={() => handleMouseEnter(i + 1)}
+          >
             {i + 1}
           </div>
         );
