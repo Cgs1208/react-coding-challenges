c5ec9c57622817ea98e509d5dc4f11f33b8b93c4
Cgs1208 <charangs157@gmail.com>
2024-03-30 23:17:47 +0530

    fetch and render employee list

--- employee-database-management/src/script.js
+++ employee-database-management/src/script.js
@@ -3,4 +3,32 @@
   const data = await response.json();
 
   const employees = data;
+  const selectedEmployeeId = employees[0].id;
+  const selectedEmployee = employees[0];
+
+  const employeeList = document.querySelector(".employees__names--list");
+  const employeeInfo = document.querySelector(".employees__single--info");
+
+  //Add employee logic
+
+  //selecting employee logic
+
+  // Render All Employees Logic start
+  const renderEmployees = () => {
+    employeeList.innerHTML = "";
+    employees.forEach((emp) => {
+      const employee = document.createElement("span");
+      employee.classList.add("employees__names--item");
+      if (parseInt(selectedEmployeeId, 10) === emp.id) {
+        employee.classList.add("selected");
+        selectedEmployee = emp;
+      }
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i>`;
+      employeeList.append(employee);
+    });
+  };
+  // Render All Employees Logic end
+
+  renderEmployee();
 })();
