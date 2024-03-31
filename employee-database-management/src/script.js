022889434ae6ddc6f9d2e0af8ba9a7fc75baad05
Cgs1208 <charangs157@gmail.com>
2024-03-31 20:15:02 +0530

    Reneder single employee info

--- employee-database-management/src/script.js
+++ employee-database-management/src/script.js
@@ -34,7 +34,28 @@
     if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
       selectedEmployeeId = e.target.id;
       renderEmployees();
-      //renderSingleEmployee();
+      renderSingleEmployee();
     }
   });
+
+  // Render Single Employee Logic - START
+  const renderSingleEmployee = () => {
+    // Employee Delete Logic - START
+    if (selectedEmployeeId === -1) {
+      employeeInfo.innerHTML = "";
+      return;
+    }
+    // Employee Delete Logic - END
+
+    employeeInfo.innerHTML = `
+      <img src="${selectedEmployee.imageUrl}" />
+      <span class="employees__single--heading">
+      ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
+      </span>
+      <span>${selectedEmployee.address}</span>
+      <span>${selectedEmployee.email}</span>
+      <span>Mobile - ${selectedEmployee.contactNumber}</span>
+      <span>DOB - ${selectedEmployee.dob}</span>
+    `;
+  };
 })();
