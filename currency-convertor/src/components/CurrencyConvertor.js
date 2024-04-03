6347ea98b64fb2f758c1430c8e22c88b14531777
Cgs1208 <charangs157@gmail.com>
2024-04-03 22:30:25 +0530

    currencies API's from https://www.frankfurter.app/docs/

--- currency-convertor/src/components/CurrencyConvertor.js
+++ currency-convertor/src/components/CurrencyConvertor.js
@@ -1,4 +1,6 @@
 import React from "react";
+//currencies = https://api.frankfurter.app/currencies
+//currencies conversion = https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
 
 const CurrencyConvertor = () => {
   return <div>CurrencyConvertor</div>;
