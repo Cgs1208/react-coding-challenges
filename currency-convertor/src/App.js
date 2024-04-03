e653d3734b4d974a0db657e8d04584e5de3856e3
Cgs1208 <charangs157@gmail.com>
2024-04-03 22:11:30 +0530

    initial commit

---
+++ currency-convertor/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CurrencyConvertor from "./components/CurrencyConvertor";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+      <CurrencyConvertor />
+    </div>
+  );
+}
+
+export default App;
