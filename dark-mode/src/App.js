46d7edfa70d93271dfbc4f51090189d9ead9baf0
Cgs1208 <charangs157@gmail.com>
2024-04-15 22:40:40 +0530

    new product page Route

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
+import Product from "./pages/product";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./theme-context";
 import Blog from "./pages/Blog";
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
+            <Route path="/product" element={<Product />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
