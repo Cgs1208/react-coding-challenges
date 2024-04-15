dc437fe9f87e3cd2375e3f3c99ae6d01d75aad56
Cgs1208 <charangs157@gmail.com>
2024-04-15 22:41:26 +0530

    product page file

---
+++ dark-mode/src/pages/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <h1>Product page</h1>
+      <h2>Welcome to Product Page</h2>
+    </div>
+  );
+};
+
+export default Product;
