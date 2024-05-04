41702df8ea1a223892e73794dff5e7a1340e737b
Cgs1208 <charangs157@gmail.com>
2024-05-04 13:28:16 +0530

    final UI built

--- currency-convertor/src/components/CurrencyConvertor.js
+++ currency-convertor/src/components/CurrencyConvertor.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-//currencies = https://api.frankfurter.app/currencies
+//currencies = https://api.frankfurter.app/currencies - from frankfurter.app
 //currencies conversion = https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
 
 const CurrencyConvertor = () => {
-  const [currnecies, setCurrencies] = useState([]);
-  const [amount, setAmount] = useState(1);
+  const [currency, setCurrency] = useState([]);
+  const [convertedAmount, setConvertedAmount] = useState(1);
 
   return (
     <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
@@ -21,14 +21,15 @@ const CurrencyConvertor = () => {
         </label>
         <input
           type="number"
-          onChange={(e) => setAmount(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1"
+          value={convertedAmount}
+          onChange={(e) => setConvertedAmount(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 hover:ring-indigo-500 mt-1"
         />
       </div>
 
-      <div className="flex justify-emd mt-7">
+      <div className="flex justify-end mt-6">
         <button
-          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700`}
         >
           Convert
         </button>
