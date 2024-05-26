bde9ac02ded161ca3688a29d15c1cba77d9a119f
Cgs1208 <charangs157@gmail.com>
2024-05-26 20:36:43 +0530

    final commit

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setCounter((prevState) => prevState + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+      setCounter(0);
+    };
+  }, [isRunning]);
 
   const handleCounter = () => {
     setIsRunning(!isRunning);
