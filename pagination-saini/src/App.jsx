ab836d52d04325486766a39e59262c9255e4dd55
Cgs1208 <charangs157@gmail.com>
2025-03-09 23:40:18 +0530

    calculation of page numbers and size

--- pagination-saini/src/App.jsx
+++ pagination-saini/src/App.jsx
@@ -2,8 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Product from "./components/Product";
 
+const PRODUCTS_PER_PAGE = 10;
+
 function App() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const totalPages = Math.ceil(products.length * PRODUCTS_PER_PAGE);
+  const start = currentPage * PRODUCTS_PER_PAGE;
+  const end = start + PRODUCTS_PER_PAGE;
 
   const fetchData = async () => {
     const data = await fetch("https://dummyjson.com/products");
@@ -18,10 +25,17 @@ function App() {
   return !products.length > 0 ? (
     <span>No products fetched</span>
   ) : (
-    <div className="products-container">
-      {products.map((prod) => (
-        <Product key={prod.id} image={prod.thumbnail} title={prod.title} />
-      ))}
+    <div>
+      <div className="products-container">
+        {products.slice(start, end).map((prod) => (
+          <Product key={prod.id} image={prod.thumbnail} title={prod.title} />
+        ))}
+      </div>
+      <div className="page-container">
+        {[...Array(10).keys()].map((n) => (
+          <span className="page-num">{n}</span>
+        ))}
+      </div>
     </div>
   );
 }
