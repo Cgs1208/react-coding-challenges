1f666e608da0aa060d1d47399bf72c3b457408a7
Cgs1208 <charangs157@gmail.com>
2025-03-11 23:41:31 +0530

    caching results in an object

--- autocomplete-search-bar/src/App.jsx
+++ autocomplete-search-bar/src/App.jsx
@@ -7,11 +7,19 @@ function App() {
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
   const [resultsVisibility, setResultsVisibility] = useState(false);
+  const [cache, setCache] = useState({});
 
   const fetchResults = async () => {
+    if (cache[input]) {
+      setResults(cache[input]);
+      console.log("cache returned for " + input);
+      return;
+    }
+
     const data = await fetch(URL + input);
     const jsonData = await data.json();
     setResults(jsonData?.recipes);
+    setCache((prev) => ({ ...prev, [input]: jsonData?.recipes }));
   };
 
   useEffect(() => {
