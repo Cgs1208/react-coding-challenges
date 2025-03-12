3b215c8eeaa4701b16bd5acf8d7d4b79f287a7e5
Cgs1208 <charangs157@gmail.com>
2025-03-12 23:29:33 +0530

    making a separate state for all items in folder structure for children visibility

--- folder-structure/src/components/List.jsx
+++ folder-structure/src/components/List.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 
 const List = ({ list }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState({});
 
   return (
     <div className="container">
       {list.map((node) => (
         <div key={node.id} style={{ marginBottom: "5px" }}>
-          <span onClick={() => setIsExpanded((prev) => !prev)}>
+          <span
+            onClick={() =>
+              setIsExpanded((prev) => ({
+                ...prev,
+                [node.name]: !prev[node.name],
+              }))
+            }
+          >
             {node.name}
           </span>
-          {node.children && <span> {isExpanded ? "-" : "+"}</span>}
-          {isExpanded && node?.children && <List list={node.children} />}
+          {node.children && <span> {isExpanded[node.name] ? "-" : "+"}</span>}
+          {isExpanded[node.name] && node?.children && (
+            <List list={node.children} />
+          )}
         </div>
       ))}
     </div>
