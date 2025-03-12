fde488ad222d3710147f0b86e2707e3a0c557cdf
Cgs1208 <charangs157@gmail.com>
2025-03-12 23:26:16 +0530

    basic state to control showing of children for folder

--- folder-structure/src/components/List.jsx
+++ folder-structure/src/components/List.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ list }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="container">
       {list.map((node) => (
         <div key={node.id} style={{ marginBottom: "5px" }}>
-          <span>{node.name}</span>
-          {node.children && <List list={node.children} />}
+          <span onClick={() => setIsExpanded((prev) => !prev)}>
+            {node.name}
+          </span>
+          {node.children && <span> {isExpanded ? "-" : "+"}</span>}
+          {isExpanded && node?.children && <List list={node.children} />}
         </div>
       ))}
     </div>
